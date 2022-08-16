5491554abddc9b2487623bd809edaf781236e991
shinichi <skato21r@iris.eonet.ne.jp>
2022-08-16 20:32:45 +0900

    review加筆

--- src/components/Chatbot/engine/naming-state-machine.js
+++ src/components/Chatbot/engine/naming-state-machine.js
@@ -5,6 +5,27 @@ naming state machine
 ==========================================
 現在の状態を保持し、ユーザ入力から次のチャットボットの行動を決定して返す。
 
+まずpattern-encoderなどでユーザからの入力をコード化する。このとき辞書の形式を
+in-outではなくin-intentとして、encoderからは行番号でなくcodeを受け取る。
+{
+  in: ["^ねえ(.+?)さん"],
+  intent: "summon"
+},
+{
+  in: ["*"],
+  intent: "u_*"
+}
+
+usage:
+let m = new NamingStateMachine();
+
+let state = m.run(code)
+
+codeには以下の情報を格納する
+{
+  index: スクリプト中でヒットした行番号,
+  harvests: その行の正規表現に()があれば抽出内容を格納,
+}
 
 文字列の解析にはプッシュダウン・オートマトンを用い、状態遷移は以下にBNF記法でしめす。
 BNF記法はこのサイトで可視化できる。https://www.bottlecaps.de/rr/ui
@@ -39,6 +60,24 @@ const STATE_TABLES = parseTables({
 
 const DISPATCH_TABLES = dispatchTables(STATE_TABLES);
 
+function assignPos(code, currentState) {
+  const [table, state] = currentState;
+
+  for (let st in DISPATCH_TABLES[table][state]) {
+    if (code===st) {
+      return st
+    }
+  }
+  return '*'
+}
+
 export default class NamingStateMachine {
-  
+  constructor(names){
+    this.states=[['main',0]];
+    this.names=[...names];
+  }
+
+  run(code){
+    
+  }
 }
